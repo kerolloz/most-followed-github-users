97d3a3c003d42aa438090699c0b4c94629325a61
Kerollos Magdy <kerolloz@yahoo.com>
2023-04-04 13:36:18 +0200

    add star, issue and follow links to footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -3,9 +3,36 @@
     <div class="row">
       <div class="column">
         <div class="ui inverted">
-          An OpenSource Project By <b>Kerollos Magdy</b>.
+          An OpenSource Project By <a
+            href="http://github.com/kerolloz"
+            target="_blank"
+            rel="noopener noreferrer"><b>Kerollos Magdy</b></a
+          >.
         </div>
-        <div class="ui divider" />
+        <div class="ui inverted link list">
+          <a
+            class="github-button item"
+            href="https://github.com/kerolloz/most-followed-github-users"
+            data-icon="octicon-star"
+            data-show-count="true"
+            aria-label="Star kerolloz/most-followed-github-users on GitHub"
+            >Star</a
+          >
+          <a
+            class="github-button item"
+            href="https://github.com/kerolloz/most-followed-github-users/issues"
+            data-icon="octicon-issue-opened"
+            data-show-count="true"
+            aria-label="Issue kerolloz/most-followed-github-users on GitHub"
+            >Issue</a
+          >
+          <a
+            class="github-button item"
+            href="https://github.com/kerolloz"
+            data-show-count="true"
+            aria-label="Follow @kerolloz on GitHub">Follow @kerolloz</a
+          >
+        </div>
       </div>
     </div>
   </div>
